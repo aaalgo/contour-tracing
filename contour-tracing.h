1a8bab96bef2d7482788607958d24f58a3743e20
Wei Dong <wdong@wdong.org>
2015-06-05 21:25:26 -0400

    make static

--- contour-tracing.h
+++ contour-tracing.h
@@ -77,7 +77,7 @@ namespace cv {
         }
     }
 
-    void MoorContourTracing (Mat const &image, Point const &ref, vector<Point> *contour) {
+    static inline void MoorContourTracing (Mat const &image, Point const &ref, vector<Point> *contour) {
         using namespace contour_tracing_impl;
         BOOST_VERIFY(image.channels() == 1);
         switch (image.depth()) {
